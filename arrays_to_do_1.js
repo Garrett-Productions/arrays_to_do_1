f2889c8c87086170e964324b6859e5e27d7f0cf2
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-11-11 21:28:00 -0600

    first commit

--- arrays_to_do_1.js
+++ arrays_to_do_1.js
@@ -76,6 +76,23 @@ function zipIt(one_arr, two_arr){
 console.log(zipIt([4,15,100], [10,20,30,40]))
 
 
-function popFront(arr){
+
+
+
+
+
+function pop(arr){
     // save the first value in the array
+    let firstValue = arr[0]
+    console.log(arr, "array to start")
+    console.log(firstValue, "zero position")
+    //shift values to the right 
+    for(let i = 0; i <arr.length; i++){
+        arr[i] = arr[i+1] // set this position equal to the next 
+    }
+    arr.length = arr.length - 1
+    // arr.push(firstValue)
+    console.log(arr, "array has been subtracted")
+    return firstValue
 }
+console.log(pop([0,1,2,3])) 
